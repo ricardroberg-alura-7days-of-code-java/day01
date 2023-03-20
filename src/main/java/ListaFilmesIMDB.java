f775737ab7e4e05253b5b2b780e84504d334d91c
Ricard Roberg <ricardroberg@gmail.com>
2023-03-20 14:35:20 -0300

    Código finalizado. Utilizei ajuda do ChatGPT na query da API pois ainda não havia estudado sobre o assunto

--- src/main/java/ListaFilmesIMDB.java
+++ src/main/java/ListaFilmesIMDB.java
@@ -1,40 +1,78 @@
-import com.sun.deploy.net.HttpRequest;
-import sun.net.www.http.HttpClient;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ListaFilmesIMDB {
     public static void main(String[] args) throws IOException {
 
-        // Searcg Title https://imdb-api.com/en/API/SearchTitle/<$IMDB_KEY>/<$title>
-        // TOP 250 movies https://imdb-api.com/en/API/Top250Movies/k_uxt1ijvp
-        // TOP 250 TVs https://imdb-api.com/en/API/Top250TVs/k_uxt1ijvp
-        // Most Popular Movies https://imdb-api.com/en/API/MostPopularMovies/k_uxt1ijvp
-        // Most Popular Tvs https://imdb-api.com/en/API/MostPopularTVs/k_uxt1ijvp
-        // Comming soon https://imdb-api.com/en/API/ComingSoon/k_uxt1ijvp
+        Scanner sc = new Scanner(System.in);
 
+        final String MOST_POPULAR_TV = "MostPopularTVs/";
+        final String MOST_POPULAR_MOVIE = "MostPopularMovies/";
+        final String TOP_250_TV = "Top250TVs/";
+        final String TOP_250_MOVIE = "Top250Movies/";
+
+        String SearchField = "";
+        String titleName = "";
+        int consultas = 10;
+        int opcao = 0;
+
+        while (opcao != 6) {
+            System.out.println("=================================== # de Resltados: " + consultas);
+            System.out.println("1 - Alterar número de resultados");
+            System.out.println("2 - Exibir lista Top 250 Filmes");
+            System.out.println("3 - Exibir lista Top 250 Séries TV");
+            System.out.println("4 - Filmes mais populares");
+            System.out.println("5 - Séries TV mais populares");
+            System.out.println("6 - Sair");
+            System.out.println("====================================================");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            if (opcao == 1) {
+                System.out.println("Informe a quantidade de resultados da pesquisa  (0 para Todos os Resultados): ");
+                consultas = sc.nextInt();
+                sc.nextLine();
+            } else if (opcao == 2) {
+                SearchField = TOP_250_MOVIE;
+                titleName = "";
+            } else if (opcao == 3) {
+                SearchField = TOP_250_TV;
+                titleName = "";
+            } else if (opcao == 4) {
+                SearchField = MOST_POPULAR_MOVIE;
+                titleName = "";
+            } else if (opcao == 5) {
+                SearchField = MOST_POPULAR_TV;
+                titleName = "";
+            }
+
+            if (opcao != 1 && opcao != 6) {
+                ReturnSearch(SearchField, titleName, consultas);
+            }
+        }
+    }
+
+    public static void ReturnSearch(String SearchField, String titleName, int consultas) throws IOException {
+        // Uma pequena ajuda do CHATGPT
         Dotenv dotenv = Dotenv.load();
         String api_key = dotenv.get("APIKEY");
-        String query = "inception";
-        String url = "https://imdb-api.com/pt/API/SearchTitle/" + api_key + "/" + query;
+        String url = "https://imdb-api.com/pt/API/" + SearchField + api_key + "/" + titleName;
 
-        // Create a URL object from the request URL
         URL imdbApiUrl = new URL(url);
 
-        // Open a connection to the IMDb API endpoint
         HttpURLConnection con = (HttpURLConnection) imdbApiUrl.openConnection();
         con.setRequestMethod("GET");
 
-        // Set the User-Agent header to identify your application
         con.setRequestProperty("User-Agent", "My Java Application");
 
-        // Read the response from the IMDb API endpoint
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuilder response = new StringBuilder();
@@ -43,8 +81,30 @@ public class ListaFilmesIMDB {
         }
         in.close();
 
-        // Print the response from the IMDb API endpoint
-        System.out.println(response.toString());
+
+        // Convertendo de volta para JSON pois não consegui iterar sobre StringBuilder
+        JSONObject json = new JSONObject(response.toString());
+        JSONArray results = json.getJSONArray("items");
+
+        if (consultas == 0 || results.length() < consultas) consultas = results.length();
+
+        for (int i = 0; i < consultas; i++) {
+            JSONObject result = results.getJSONObject(i);
+            String crew = result.getString("crew");
+            String title = result.getString("title");
+            String year = result.getString("year");
+            String rank = result.getString("rank");
+
+
+            System.out.println("POSIÇÃO: " + rank);
+            System.out.println("TITLE: " + title);
+            System.out.println("ANO: " + year);
+            System.out.println("ELENCO: " + crew);
+            System.out.println();
+        }
+
 
     }
+
 }
+
